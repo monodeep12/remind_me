83298235509d798eab4f1f1e9b5ce0886391e87b
Mono <monodeep12@gmail.com>
2016-06-25 19:12:58 +0530

    Implemented queuing of reminders using celery + redis

--- reminder/reminder/settings/common.py
+++ reminder/reminder/settings/common.py
@@ -27,6 +27,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
+# Address of Redis instance - Celery broker
+BROKER_URL = 'redis://localhost:6379/0'
+BROKER_POOL_LIMIT = 8
+
+# Reminder time: how early text messages are sent in advance of reminder time
+REMINDER_TIME = 30 # seconds
+
 
 # Application definition
 DJANGO_APPS = (
@@ -107,3 +114,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR + '/staticfiles'
+
+CELERY_ALWAYS_EAGER = False
