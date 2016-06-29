718e765b81b02319d406d667896a2834e34a78ee
Mono <monodeep12@gmail.com>
2016-06-29 22:30:04 +0530

    Removed duplicate code

--- reminder/tasks/tests.py
+++ reminder/tasks/tests.py
@@ -18,11 +18,8 @@ else:
         
 
 class RemindersTest(TestCase):
-
-    def test_str(self):
-        # Arrange
-        t=datetime.datetime.now() + datetime.timedelta(minutes=10)
-        reminder = mommy.make(
+    t=datetime.datetime.now() + datetime.timedelta(minutes=10)
+    reminder = mommy.make(
             Reminders,
             id="2662c2d9f3414589b63d7cee423dd37d",
             email="mono@amagi.com",
@@ -30,61 +27,23 @@ class RemindersTest(TestCase):
             date=datetime.datetime.now().date(),
             time=t.time(),
             message="Test message")
-
-        # Assert
-        self.assertEqual(str(reminder), "2662c2d9f3414589b63d7cee423dd37d")
-
-    # def test_clean_invalid_time(self):
-    #     # Arrange
-    #     t = datetime.datetime.now() - datetime.timedelta(minutes = 10)
-    #     reminder = mommy.make(Reminders,
-    #     email="mono@amagi.com",
-    #     phone_number="+919945055794",
-    #     date=datetime.datetime.now().date(),
-    #     time=t.time(),
-    #     message="Test message")
-
-    #     # Assert
-    #     with self.assertRaises(ValidationError):
-    #         reminder.clean()
+            
+    def test_str(self):
+        self.assertEqual(str(self.reminder), "2662c2d9f3414589b63d7cee423dd37d")
 
     def test_clean_valid_reminder(self):
-        # Arrange
-        t=datetime.datetime.now() + datetime.timedelta(minutes=10)
-        reminder = mommy.make(
-            Reminders,
-            email="mono@amagi.com",
-            phone_number="+919945055794",
-            date=datetime.datetime.now().date(),
-            time=t.time(),
-            message="Test message")
-        
-        # Assert
         try:
-            reminder.clean()
+            self.reminder.clean()
         except ValidationError:
             self.fail('reminder with time in the past raised ValidationError')
 
     def test_schedule_reminder(self):
-        # Arrange
-        t=datetime.datetime.now() + datetime.timedelta(minutes=10)
-        reminder = mommy.make(
-            Reminders,
-            email="mono@amagi.com",
-            phone_number="+919945055794",
-            date=datetime.datetime.now().date(),
-            time=t.time(),
-            message="Test message")
-
-        # Act
         with patch.object(send_reminder, 'apply_async') as mock:
-            reminder.schedule_reminder()
+            self.reminder.schedule_reminder()
 
-        # Assert
         self.assertTrue(mock.called)
 
     def test_save_initial_creation(self):
-        # Act
         with patch.object(Reminders, 'schedule_reminder', return_value=123) as mock:
             t=datetime.datetime.now() + datetime.timedelta(minutes=10)
             reminder = mommy.make(
@@ -95,6 +54,5 @@ class RemindersTest(TestCase):
                 time=t.time(),
                 message="Test message")
 
-        # Assert
         self.assertTrue(mock.called)
         self.assertEqual(reminder.task_id, 123)
